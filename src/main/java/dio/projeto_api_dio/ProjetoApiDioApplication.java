4caa7d6e971526214fbf7a1f3a0795978175640a
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-06-03 11:09:24 -0300

    first commit

---
+++ src/main/java/dio/projeto_api_dio/ProjetoApiDioApplication.java
@@ -0,0 +1,13 @@
+package dio.projeto_api_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiDioApplication.class, args);
+	}
+
+}
